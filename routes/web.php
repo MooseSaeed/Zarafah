fed0a48d7cc38b59d862c2a666123856d2df3605
Moose <mostafasaid1994@gmail.com>
2022-01-13 19:09:49 +0200

    Modifying routes

--- routes/web.php
+++ routes/web.php
@@ -13,18 +13,26 @@ Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 
 Route::post('newsletter', NewsletterController::class);
 
+// Route Group to MiddleWare Admin
+
 Route::group(['middleware' => 'admin'], function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->middleware(['auth'])->name('dashboard');
 
-    Route::get('admin/posts/create', [AdminPostController::class, 'create']);
+    //Route Resource Routing Technique
+
+    Route::resource('admin/posts', AdminPostController::class);
+
+    //Old Routing Technique
+
+    /*  Route::get('admin/posts/create', [AdminPostController::class, 'create']);
     Route::post('admin/posts', [AdminPostController::class, 'store']);
 
     Route::get('admin/posts', [AdminPostController::class, 'index']);
     Route::get('admin/posts/{post}/edit', [AdminPostController::class, 'edit']);
     Route::patch('admin/posts/{post}', [AdminPostController::class, 'update']);
-    Route::delete('admin/posts/{post}', [AdminPostController::class, 'destroy']);
+    Route::delete('admin/posts/{post}', [AdminPostController::class, 'destroy']); */
 });
 
 require __DIR__ . '/auth.php';
