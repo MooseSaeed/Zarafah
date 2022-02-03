e250927da5a1ea4c9a7252814fb8b7cde79737b6
Mostafa Said <94674993+MooseSaeed@users.noreply.github.com>
2022-02-03 13:41:40 +0200

    fixing tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-    content: ["./resources/**/*.blade.php", "./resources/**/*.js"],
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/views/**/*.blade.php",
+    ],
 
     theme: {
         extend: {
